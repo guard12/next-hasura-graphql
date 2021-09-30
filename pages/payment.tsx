782d6df634881b7e01e6a0d2c9f20255cd2afde8
Matus Kollarcik <mko@coinify.com>
2021-09-30 12:26:49 +0200

    add more controls

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -3,48 +3,69 @@ import { useState } from 'react'
 import { Slider, InputNumber, Row, Col } from 'antd';
 
 const Payment: NextPage = () => {
-  const [width, setWidth] = useState(0)
-  const [height, setHeight] = useState(0)
+  const [width, setWidth] = useState(400)
+  const [height, setHeight] = useState(800)
+  const [background, setBackground] = useState("fff")
   return <div>
-    <Row>
-      <Col span={6}>
-        <Slider
-          min={1}
-          max={1000}
-          onChange={(value) => setHeight(value)}
-          value={typeof height === 'number' ? height : 0}
-        />
-        <InputNumber
-          min={0}
-          max={1000}
-          style={{ margin: '0 16px' }}
-          step={50}
-          value={height}
-          onChange={(value) => setHeight(value)}
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col span={6}>
-        <Slider
-          min={1}
-          max={1000}
-          onChange={(value) => setWidth(value)}
-          value={typeof width === 'number' ? width : 0}
-        />
-        <InputNumber
-          min={0}
-          max={1000}
-          style={{ margin: '0 16px' }}
-          step={50}
-          value={width}
-          onChange={(value) => setWidth(width)}
-        />
-      </Col>
-    </Row>
-
-    <div style={{ backgroundColor: 'lightgray', display: 'flex', justifyContent: 'center', padding: '20px 0px'}}>
-      <iframe style={{ border: 'none'}} width={`${width}px`} height={`${height}px`} src="http://localhost:3000/demo/payment" />
+    <div style={{ padding: "20px", borderBottom: "1px solid black"}}>
+      <Row gutter={16}>
+      <Col span={1}>
+          Height:
+        </Col>
+        <Col span={6}>
+          <Slider
+            min={1}
+            max={1000}
+            onChange={(value) => setHeight(value)}
+            value={typeof height === 'number' ? height : 0}
+          />
+          <InputNumber
+            min={0}
+            max={1000}
+            style={{ margin: '0 16px' }}
+            step={50}
+            value={height}
+            onChange={(value) => setHeight(value)}
+          /> px
+        </Col>
+        <Col span={2}>
+          Background:
+        </Col>
+        <Col span={1}>
+          <div style={{ width: "40px", height: "40px", backgroundColor: "#20232a", color: "white" }} onClick={() => setBackground("#20232a")}>Dark</div>
+        </Col>
+        <Col span={1}>
+          <div style={{ width: "40px", height: "40px", backgroundColor: "white", color: "black", border: "1px solid black" }} onClick={() => setBackground("white")}>Light</div>
+        </Col>
+        <Col span={1}>
+          <div style={{ width: "40px", height: "40px", backgroundColor: "#80808014", color: "black" }} onClick={() => setBackground("#80808014")}>Gray</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={1}>
+          Width:
+        </Col>
+        <Col span={6}>
+          <Slider
+            min={1}
+            max={1000}
+            onChange={(value) => setWidth(value)}
+            value={typeof width === 'number' ? width : 0}
+          />
+          <InputNumber
+            title="Width"
+            min={0}
+            max={1000}
+            style={{ margin: '0 16px' }}
+            step={50}
+            value={width}
+            onChange={(value) => setWidth(value)}
+          /> px
+        </Col>
+      </Row>
+    </div>
+    <div style={{ backgroundColor: background, display: 'flex', justifyContent: 'center', padding: '20px 0px' }}>
+      <iframe style={{ border: 'none' }} width={`${width}px`} height={`${height}px`} src={process.env.NEXT_PUBLIC_PAYMENT_URL} />
     </div>
   </div>
 }
